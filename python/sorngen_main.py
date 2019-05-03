ccac1a16f049078eda2bd476d9e76cdea8b2ed16
mbaerthel <50237416+mbaerthel@users.noreply.github.com>
2019-05-03 15:56:56 +0200

    Add files via upload

--- python/sorngen_main.py
+++ python/sorngen_main.py
@@ -28,12 +28,12 @@ print("##")
 print("###################################################")
 
 ## 0/ check for existing folder structure
-if not os.path.exists(".\VHDL"):
+if not os.path.exists("./VHDL"):
 	# create subfolder ".\VHDL"
-	os.makedirs(".\VHDL")
-if not os.path.exists(".\VHDL\VHDLbasic"):
-	# create subfolder ".\VHDL\VHDLbasic"
-	os.makedirs(".\VHDL\VHDLbasic")
+	os.makedirs("./VHDL")
+if not os.path.exists("./VHDL/VHDLbasic"):
+	# create subfolder "./VHDL/VHDLbasic"
+	os.makedirs("./VHDL/VHDLbasic")
 
 ## 1/ read data
 # 1.1/ initialize global sorngen environment
